8f960a5fa9d8f3565f614049871be66be9a77cb3
kangdora <ioprt123@gmail.com>
2024-10-08 17:20:53 +0900

    feat:problem4 solve

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,17 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  word = word.split("");
+  ans = [];
+
+  for (let i of word) {
+    let asc = i.charCodeAt(0);
+    if (65 <= asc && asc <= 90) {
+      ans.push(String.fromCharCode(155 - asc));
+    } else if (97 <= asc && asc <= 122) {
+      ans.push(String.fromCharCode(219 - asc));
+    } else ans.push(i);
+  }
+
+  return ans.join("");
 }
 
 module.exports = problem4;
