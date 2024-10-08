eeeb63458980a8c8b6ee997a2d05d46170246663
kangdora <ioprt123@gmail.com>
2024-10-08 17:28:46 +0900

    feat:porblem5 solve

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,11 @@
 function problem5(money) {
-  var answer;
+  won = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let answer = new Array(9);
+
+  for (let i = 0; i < won.length; i++) {
+    answer[i] = Math.floor(money / won[i]);
+    money %= won[i];
+  }
   return answer;
 }
 
