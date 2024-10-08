e78175190d4dffc2a5042e2dbe1add7fb5ef81f2
kangdora <ioprt123@gmail.com>
2024-10-08 15:41:10 +0900

    feat: problem1 solve

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,34 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+    if (pobi[0] + 1 === pobi[1] && crong[0] + 1 === crong[1]){
+        const po = compare(pobi);
+        const cr = compare(crong);
+        if (po > cr) {
+            return 1;
+        } else if (po < cr) {
+            return 2;
+        } else if (po === cr) {
+            return 0;
+        }
+    } else return -1;
+}
+
+function compare(lst) {
+    let ans = 0;
+    
+    for (let i = 0; i < lst.length; i++) {
+        let a = 0;
+        let b = 1;
+        let num = lst[i];
+        
+        while (num !== 0) {
+            const digit = num % 10;
+            a += digit;
+            b *= digit;
+            num = Math.floor(num / 10);
+        }
+        ans = Math.max(ans, Math.max(a, b));
+    }
+    return ans;
 }
 
 module.exports = problem1;
