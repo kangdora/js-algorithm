2a55353e60676d2e19dfa40cb9979b4b3381fa6c
kangdora <ioprt123@gmail.com>
2024-10-08 18:40:12 +0900

    feat: problem6 solve

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,28 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  let dict = new Map();
+  let dup = new Map();
+  let ans = new Set();
+
+  for (element of forms) {
+    const [K, V] = element;
+    dict.set(K, V);
+  }
+
+  for (let [K, V] of dict) {
+    for (let i = 0; i < V.length - 1; i++) {
+      let char = V.slice(i, i + 2);
+
+      if (dup.has(char)) {
+        ans.add(K);
+        ans.add(dup.get(char));
+      } else dup.set(char, K);
+    }
+  }
+
+  ans = [...ans].sort((a, b) => a - b);
+  ans.sort((a, b) => a - b);
+
+  return ans;
 }
 
 module.exports = problem6;
