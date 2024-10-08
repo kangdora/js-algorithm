4aacd0b8fcef1b2a091528a42f3a4702b158afbc
kangdora <ioprt123@gmail.com>
2024-10-08 16:25:29 +0900

    feat: problem2 solve

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,18 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  var lst = cryptogram.split("");
+  var previous = 0;
+
+  while (lst.length !== previous) {
+    previous = lst.length;
+
+    for (let i = 0; i < lst.length - 1; i++) {
+      if (lst[i] === lst[i + 1]) {
+        lst.splice(i, 2);
+        i--;
+      }
+    }
+  }
+  return lst.join("");
 }
 
 module.exports = problem2;
