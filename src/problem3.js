ca1ea0e1aad41af30e41f44601dc2596e8263b5f
kangdora <ioprt123@gmail.com>
2024-10-08 16:35:51 +0900

    feat:problem3 solve

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,15 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let cnt = 0;
+
+  for (let i = 1; i <= number; i++) {
+    let str = String(i).split("");
+    for (let j = 0; j < number; j++) {
+      if (str[j] == 3 || str[j] == 6 || str[j] == 9) {
+        cnt += 1;
+      }
+    }
+  }
+  return cnt;
 }
 
 module.exports = problem3;
